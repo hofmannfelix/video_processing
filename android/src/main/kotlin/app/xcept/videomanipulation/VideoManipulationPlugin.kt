828850639ccb550be38a343805ca65e9dbb4e26b
Felix Hofmann <hofmannfelix@ymail.com>
2019-06-08 16:58:20 +0200

    Updated method declaration

--- android/src/main/kotlin/app/xcept/videomanipulation/VideoManipulationPlugin.kt
+++ android/src/main/kotlin/app/xcept/videomanipulation/VideoManipulationPlugin.kt
@@ -6,22 +6,29 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class VideoManipulationPlugin: MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "video_manipulation")
-      channel.setMethodCallHandler(VideoManipulationPlugin())
+class VideoManipulationPlugin : MethodCallHandler {
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val channel = MethodChannel(registrar.messenger(), "video_manipulation")
+            channel.setMethodCallHandler(VideoManipulationPlugin())
+        }
     }
-  }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else if (call.method == "generateVideo") {
-      result.success(null)
-    } else {
-      result.notImplemented()
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else if (call.method == "generateVideo") {
+            result.success(null)
+        } else {
+            result.notImplemented()
+        }
     }
-  }
+}
+
+typealias Callback = () -> Boolean
+
+interface IVideoManipulation {
+
+    fun generateVideo(assetPaths: Collection<String>, outputFilename: String, outputFps: Int, outputSpeed: Double, completion: Callback);
 }
