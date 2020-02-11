e8ae590fad9df4c70c57cff86776ece89705b629
Felix Hofmann <hofmannfelix@ymail.com>
2020-02-11 12:35:33 +0200

    Implemented logic to process variable speeds with ffmpeg as well as getting the progress

--- lib/video_processing.dart
+++ lib/video_processing.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
@@ -46,7 +45,7 @@ class VideoProcessing {
       {String inputPath,
       String outputPath,
       List<VideoProcessSettings> settings,
-      ProgressStreamInitCallback onProgressStreamInitialized}) {
+      ProgressStreamInitCallback onProgressStreamInitialized}) async {
     onProgressStreamInitialized ??= (_) {};
     final taskId = outputPath;
     if (taskProgressControllers[taskId] != null) {
@@ -55,9 +54,12 @@ class VideoProcessing {
     }
     taskProgressControllers[taskId] = StreamController.broadcast();
     onProgressStreamInitialized(progressStream(taskId: taskId));
-    final settingsMap = settings.map((s) => s.asMap).toList();
+    //final settingsMap = settings.reversed.map((s) => s.asMap).toList();
     //return _channel.invokeMethod('processVideo', [inputPath, outputPath, settingsMap]);
-    return _processVideoFFMPEG(inputPath: inputPath, outputPath: outputPath, settings: settings);
+    final result = await _processVideoFFMPEG(
+        inputPath: inputPath, outputPath: outputPath, settings: settings);
+    _taskProgressControllers.remove(taskId).close();
+    return result;
   }
 
   static Future<String> processVideoWithOverlay(
@@ -79,42 +81,78 @@ class VideoProcessing {
 
   static Future<String> _processVideoFFMPEG(
       {String inputPath, String outputPath, List<VideoProcessSettings> settings}) async {
-//    var args = [
-//      "-i",
-//      '$inputPath',
-//      '-filter:v',
-//      'setpts=${1.0 / 4.0}*PTS',
-//      '-an',
-//      '$outputPath'
-//    ];
-//    var args = [
-//      '-itsscale',
-//      '${1.0 / 4.0}',
-//      "-i",
-//      '$inputPath',
-//      '-c',
-//      'copy',
-//      '$outputPath'
-//    ];
-//    await FlutterFFmpeg().executeWithArguments(args);
-//    await File(inputPath).delete();
-//    await File(outputPath).rename(inputPath);
+    String filterInput = "";
 
-    //TODO: Change settings into ffmpeg command
+    ///Video settings
+    for (int i = 0; i < settings.length; i++) {
+      final s = settings[i];
+      final start = (s.start.inMilliseconds / 1000).toStringAsFixed(3);
+      final end = (s.end.inMilliseconds / 1000).toStringAsFixed(3);
+      final speed = (1 / s.speed).toStringAsFixed(3);
+      filterInput += "[0:v]trim=$start:$end,setpts=$speed*(PTS-STARTPTS)[v${i + 1}];";
+    }
 
-    var args = [
+    ///Audio settings
+    for (int i = 0; i < settings.length; i++) {
+      final s = settings[i];
+      final start = (s.start.inMilliseconds / 1000).toStringAsFixed(3);
+      final end = (s.end.inMilliseconds / 1000).toStringAsFixed(3);
+      final speed = (s.speed).toStringAsFixed(3);
+      filterInput += "[0:a]atrim=$start:$end,asetpts=PTS-STARTPTS,atempo=$speed[a${i + 1}];";
+    }
+
+    ///Merge settings
+    for (int i = 1; i <= settings.length; i++) {
+      filterInput += "[v$i][a$i]";
+    }
+    filterInput += "concat=n=${settings.length}:v=1:a=1";
+
+    ///Init progress callback
+    var timelapseDuration = Duration.zero;
+    for (final s in settings) timelapseDuration += (s.end - s.start) * (1 / s.speed);
+    final ffmpegConfig = FlutterFFmpegConfig();
+    ffmpegConfig.resetStatistics();
+    ffmpegConfig.enableStatisticsCallback((int time, int size, double bitrate, double speed,
+        int videoFrameNumber, double videoQuality, double videoFps) {
+      final taskId = outputPath;
+      final progress = (time / timelapseDuration.inMilliseconds).clamp(0.0, 1.0);
+      _taskProgressControllers[taskId].add(progress);
+    });
+
+    ///Execute ffmpeg
+    final returnCode = await FlutterFFmpeg().executeWithArguments([
       "-i",
       '$inputPath',
       '-filter_complex',
-      '[0:v]trim=0:10,setpts=PTS-STARTPTS[v1]; [0:v]trim=10:50,setpts=0.25*(PTS-STARTPTS)[v2]; [0:v]trim=50,setpts=PTS-STARTPTS[v3]; [0:a]atrim=0:10,asetpts=PTS-STARTPTS[a1]; [0:a]atrim=10:50,asetpts=PTS-STARTPTS,atempo=4[a2]; [0:a]atrim=50,asetpts=PTS-STARTPTS[a3]; [v1][a1][v2][a2][v3][a3]concat=n=3:v=1:a=1',
-//      '-preset',
-//      'ultrafast',
-//      '-profile:v',
-//      'baseline',
+      '$filterInput',
+      //'[0:v]trim=0:10,setpts=PTS-STARTPTS[v1]; [0:v]trim=10:50,setpts=0.25*(PTS-STARTPTS)[v2]; [0:v]trim=50,setpts=PTS-STARTPTS[v3]; [0:a]atrim=0:10,asetpts=PTS-STARTPTS[a1]; [0:a]atrim=10:50,asetpts=PTS-STARTPTS,atempo=4[a2]; [0:a]atrim=50,asetpts=PTS-STARTPTS[a3]; [v1][a1][v2][a2][v3][a3]concat=n=3:v=1:a=1',
+      '-preset',
+      'superfast',
+      '-crf',
+      '30',
+      '-profile:v',
+      'baseline',
       '$outputPath',
-    ];
-
-    await FlutterFFmpeg().executeWithArguments(args);
+    ]);
+    if (returnCode != 0) {
+      final lastStats = await ffmpegConfig.getLastReceivedStatistics();
+      throw ArgumentError.value(
+        inputPath,
+        "FFmpeg Error",
+        "Could not process file at given path: $lastStats",
+      );
+    }
     return outputPath;
   }
 }
+
+//TODO: Change settings into ffmpeg command
+//    final s = """
+//    [0:v]trim=0:10,setpts=PTS-STARTPTS[v1];
+//    [0:v]trim=10:50,setpts=0.25*(PTS-STARTPTS)[v2];
+//    [0:v]trim=50,setpts=PTS-STARTPTS[v3];
+//    [0:a]atrim=0:10,asetpts=PTS-STARTPTS[a1];
+//    [0:a]atrim=10:50,asetpts=PTS-STARTPTS,atempo=4[a2];
+//    [0:a]atrim=50,asetpts=PTS-STARTPTS[a3];
+//    [v1][a1][v2][a2][v3][a3]concat=n=3:v=1:a=1,
+//    """;
